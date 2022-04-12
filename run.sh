1f229b9b44cb124c8d80c3bdab46e64656463b58
liuzhihong19911 <71239741+liuzhihong19911@users.noreply.github.com>
2022-04-12 20:33:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ wget https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09
 mv subspace-node-ubuntu-x86_64-snapshot-2022-mar-09 node
 wget https://github.com/subspace/subspace/releases/download/snapshot-2022-mar-09/subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09
 mv subspace-farmer-ubuntu-x86_64-snapshot-2022-mar-09 farmer
-chmod +x node.sh farmer.sh node farmer
+chmod +x run.sh && ./run.sh
 ./node.sh
 sleep 10
 ./farmer.sh
