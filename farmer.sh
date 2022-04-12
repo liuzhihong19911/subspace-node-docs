2a315bab952b0e2c9128c5ea4f77ac777da9a697
syskey <lopetvers@gmail.com>
2022-04-12 10:56:37 +0800

    1

---
+++ farmer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# @Author: syskey
+# @Date:   2022/4/12
+# @twitter:https://twitter.com/skyMetaverse
+
+# 将WALLET_ADDRESS修改为自己的钱包地址
+# 例子：nohup ./farmer farm --reward-address st8N65fpjgVguZr7kxVk4FMcZphZF465uzDThGhz24BPyQy4v > farmer.log 2>&1 &
+
+nohup ./farmer farm --reward-address WALLET_ADDRESS > farmer.log 2>&1 &
---
+++ node.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# @Author: syskey
+# @Date:   2022/4/12
+# @twitter:https://twitter.com/skyMetaverse
+
+# 将INSERT_YOUR_ID修改为自己要运行的节点的名字
+# 例子：nohup ./node \
+# --chain testnet \
+# --wasm-execution compiled \
+# --execution wasm \
+# --bootnodes "/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr" \
+# --rpc-cors all \
+# --rpc-methods unsafe \
+# --ws-external \
+# --validator \
+# --telemetry-url "wss://telemetry.polkadot.io/submit/ 1" \
+# --telemetry-url "wss://telemetry.subspace.network/submit 1" \
+# --name syskey \
+# > node.log 2>&1 &
+
+
+nohup ./node \
+  --chain testnet \
+  --wasm-execution compiled \
+  --execution wasm \
+  --bootnodes "/dns/farm-rpc.subspace.network/tcp/30333/p2p/12D3KooWPjMZuSYj35ehced2MTJFf95upwpHKgKUrFRfHwohzJXr" \
+  --rpc-cors all \
+  --rpc-methods unsafe \
+  --ws-external \
+  --validator \
+  --telemetry-url "wss://telemetry.polkadot.io/submit/ 1" \
+  --telemetry-url "wss://telemetry.subspace.network/submit 1" \
+  --name INSERT_YOUR_ID \
+  > node.log 2>&1 &
