bdd9eba6e2a77a8a1dc61c6081ac6d0191d1aa77
liuzhihong19911 <71239741+liuzhihong19911@users.noreply.github.com>
2022-04-13 10:56:30 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -30,5 +30,5 @@ nohup ./node \
   --validator \
   --telemetry-url "wss://telemetry.polkadot.io/submit/ 1" \
   --telemetry-url "wss://telemetry.subspace.network/submit 1" \
-  --name INSERT_YOUR_ID \
+  --name OZZZZZZ \
   > node.log 2>&1 &
